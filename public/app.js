a28508883c86c3dbcda9d3e1453aeff4ceb5b79e
Tyler Maran <tyler.maran@gmail.com>
2018-04-14 13:46:37 -0400

    updating with mongodb

--- public/app.js
+++ public/app.js
@@ -4,11 +4,10 @@ $.getJSON("/articles", function (data) {
   for (var i = 0; i < 20; i++) {
 
     // Create new article
-    
-
     var newArticle = $("<div>");
     newArticle.addClass("display-article");
     newArticle.attr("data-id", data[i]._id);
+    newArticle.attr("showNote", true);
     newArticle.text(data[i].title);
     // Add byline
     var byline = $("<div class='byline'>");
@@ -25,28 +24,44 @@ $.getJSON("/articles", function (data) {
 
     var articleRow = $("<div class='row'>")
     articleRow.attr("id", data[i]._id);
-    var articleColumn = $("<div class='col-sm-9'>")
-    var noteColumn = $("<div class='col-sm-3'>")
-    
+    var articleColumn = $("<div class='col-sm-8'>")
+
     articleColumn.append(newArticle);
     articleRow.append(articleColumn);
-    articleRow.append(noteColumn);
-
-
-
     $("#articles").append(articleRow);
-
   }
 });
 
+$(".homeButton").on("click", function () {
+  $.ajax({
+      method: "GET",
+      url: "/scrape"
+    })
+    // With that done, add the note information to the page
+    .then(function (data) {
+      console.log(data);
+    });
+})
 
 // Whenever someone clicks a p tag
 $(document).on("click", ".display-article", function () {
+
+  var showNote = $(this).attr("showNote");
+  console.log(showNote);
+  console.log(typeof (showNote));
+  if (showNote === "false") {
+    console.log("Show note is " + showNote);
+    $(".col-sm-4").empty();
+    $(this).attr("showNote", true);
+    return;
+  }
+
+  $(this).attr("showNote", false);
   // Empty the notes from the note section
   $("#notes").empty();
   // Save the id from the p tag
-  var thisId = $(this).attr("id");
-
+  var thisId = $(this).attr("data-id");
+  console.log(thisId);
   // Now make an ajax call for the Article
   $.ajax({
       method: "GET",
@@ -54,9 +69,8 @@ $(document).on("click", ".display-article", function () {
     })
     // With that done, add the note information to the page
     .then(function (data) {
-      console.log(data);
       // The title of the article
-      var newNote=$("<div>");
+      var newNote = $("<div>");
       newNote.append("<h2>" + data.title + "</h2>");
       // An input to enter a new title
       newNote.append("<input id='titleinput' name='title' >");
@@ -65,17 +79,20 @@ $(document).on("click", ".display-article", function () {
       // A button to submit a new note, with the id of the article saved to it
       newNote.append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
 
-      // $("#" + data._id).append(newNote);
-      // console.log(thisId);
-      // // If there's a note in the article
-      // if (data.note) {
-      //   // Place the title of the note in the title input
-      //   $("#titleinput").val(data.note.title);
-      //   // Place the body of the note in the body textarea
-      //   $("#bodyinput").val(data.note.body);
-      // }
+      var noteColumn = $("<div class='col-sm-4'>")
+      noteColumn.append(newNote);
+
+      $("#" + thisId).append(noteColumn);
+      // If there's a note in the article
+      if (data.note) {
+        // Place the title of the note in the title input
+        $("#titleinput").val(data.note.title);
+        // Place the body of the note in the body textarea
+        $("#bodyinput").val(data.note.body);
+      }
     });
 });
+
 
 // When you click the savenote button
 $(document).on("click", "#savenote", function () {
@@ -102,6 +119,43 @@ $(document).on("click", "#savenote", function () {
     });
 
   // Also, remove the values entered in the input and textarea for note entry
-  $("#titleinput").val("");
-  $("#bodyinput").val("");
+
 });
+
+$(".mySaved").on("click", function () {
+  $.getJSON("/articles", function (data) {
+    // For each one
+    $("#articles").html("");
+    for (var i = 0; i < 20; i++) {
+      if (data[i].note != undefined) {
+        // Create new article
+        var newArticle = $("<div>");
+        newArticle.addClass("display-article");
+        newArticle.attr("data-id", data[i]._id);
+        newArticle.attr("showNote", true);
+        newArticle.text(data[i].title);
+        // Add byline
+        var byline = $("<div class='byline'>");
+        byline.text(data[i].byline);
+        newArticle.append(byline);
+        // Add summary
+        var summary = $("<div class='summary'>");
+        summary.text(data[i].summary);
+        newArticle.append(summary);
+        // Add Link to Article
+        var link = $("<div class='link'>");
+        link.html("<a href=" + data[i].link + ' target=_"blank">Continue Reading</a>');
+        newArticle.append(link);
+
+        var articleRow = $("<div class='row'>")
+        articleRow.attr("id", data[i]._id);
+        var articleColumn = $("<div class='col-sm-8'>")
+
+        articleColumn.append(newArticle);
+        articleRow.append(articleColumn);
+        $("#articles").append(articleRow);
+      }
+
+    }
+  });
+})
--- server.js
+++ server.js
@@ -4,32 +4,6 @@ var logger = require("morgan");
 var mongoose = require("mongoose");
 var request = require("request");
 
-//lets require/import the mongodb native drivers.
-var mongodb = require('mongodb');
-
-//We need to work with "MongoClient" interface in order to connect to a mongodb server.
-var MongoClient = mongodb.MongoClient;
-
-// Connection URL. This is where your mongodb server is running.
-
-//(Focus on This Variable)
-var url = 'mongodb://<themanmaran>:<password>@ds033056.mlab.com:33056/nytscrapper';      
-//(Focus on This Variable)
-
-// Use connect method to connect to the Server
-  MongoClient.connect(url, function (err, db) {
-  if (err) {
-    console.log('Unable to connect to the mongoDB server. Error:', err);
-  } else {
-    console.log('Connection established to', url);
-
-    // do some work here with the database.
-
-    //Close connection
-    db.close();
-  }
-});
-
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
 // It works on the client and on the server
@@ -43,12 +17,14 @@ var PORT = 5000;
 // Initialize Express
 var app = express();
 
-// Configure middleware
+
 
 // Use morgan logger for logging requests
 app.use(logger("dev"));
 // Use body-parser for handling form submissions
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
@@ -56,149 +32,156 @@ app.use(express.static("public"));
 // use promises (.then syntax) instead
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://<themanmaran>:<password>@ds033056.mlab.com:33056/nytscrapper", {
-  useMongoClient: true
-});
+mongoose.connect(provess.env.MONGODB_URI || "mongodb://localhost/nytdatabase", {});
 
 // Routes
 
 // A GET route for scraping the echojs website
-app.get("/scrape", function(req, res) {
-    function scrapeNYT() {
+app.get("/scrape", function (req, res) {
+  function scrapeNYT() {
 
-        // First, tell the console what server.js is doing
-        console.log("Pulling NYT articles and logging them to Mongo Database")
-      
-        // Make a request to front page of NYT, and return the html
-        request("https://www.nytimes.com/", function (error, response, html) {
-      
-            // Load the HTML into cheerio and save it to a variable
-            // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-            var $ = cheerio.load(html);
-      
-            // An empty array to save the data that we'll scrape
-            var results = [];
-      
-            // With cheerio, find each h2-tag with the "story-heading" class
-            // (i: iterator. element: the current element)
-            $("h2.story-heading").each(function (i, element) {
-      
-              // Save the text of the element in a "title" variable
-              var title = $(element).text();
-      
-              // In the currently selected element, look at its child elements (i.e., its a-tags),
-              // then save the values for any "href" attributes that the child elements may have
-      
-              var link = $(element).children().attr("href");
-      
-              var summary = $(element).siblings(".summary").text();
-              var byline = $(element).siblings(".byline").text();
-              
-              // Save these results in an object that we'll push into the results array we defined earlier
-              results.push({
-                title: title.trim(),
-                link: link,
-                summary: summary.trim(),
-                byline: byline.trim()
-              });
-              // Call sorting function and pass results
-              sortArticles(results);
-            });
-          });
-        }
-      
-        // Log the results once you've looped through each of the elements found with cheerio
-        function sortArticles(results) {
-          console.log("-------------------------------------------------");
-          
-          // Only pulling results with full detail
-          var filteredResults = [];
-          for (var i = 0; i < results.length; i++) {
-            if (results[i].byline != "") {
-              filteredResults.push(results[i]);
-            }
-          }
-          updateDB(filteredResults);
-        }
-      
-      scrapeNYT();
+    // First, tell the console what server.js is doing
+    console.log("Pulling NYT articles and logging them to Mongo Database")
 
-      // Create a new Article using the `result` object built from scraping
-      function updateDB(filteredResults) {
-        db.Article.create(filteredResults)
-        .then(function(dbArticle) {
-          // View the added result in the console
-          console.log(dbArticle);
-        })
-        .catch(function(err) {
-          // If an error occurred, send it to the client
-          return res.json(err);
+    // Make a request to front page of NYT, and return the html
+    request("https://www.nytimes.com/", function (error, response, html) {
+
+      // Load the HTML into cheerio and save it to a variable
+      // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+      var $ = cheerio.load(html);
+
+      // An empty array to save the data that we'll scrape
+      var results = [];
+
+      // With cheerio, find each h2-tag with the "story-heading" class
+      // (i: iterator. element: the current element)
+      $("h2.story-heading").each(function (i, element) {
+
+        // Save the text of the element in a "title" variable
+        var title = $(element).text();
+
+        // In the currently selected element, look at its child elements (i.e., its a-tags),
+        // then save the values for any "href" attributes that the child elements may have
+
+        var link = $(element).children().attr("href");
+
+        var summary = $(element).siblings(".summary").text();
+        var byline = $(element).siblings(".byline").text();
+
+        // Save these results in an object that we'll push into the results array we defined earlier
+        results.push({
+          title: title.trim(),
+          link: link,
+          summary: summary.trim(),
+          byline: byline.trim()
         });
+        // Call sorting function and pass results
+        sortArticles(results);
+      });
+    });
+  }
+
+  // Log the results once you've looped through each of the elements found with cheerio
+  function sortArticles(results) {
+    console.log("-------------------------------------------------");
+
+    // Only pulling results with full detail
+    var filteredResults = [];
+    for (var i = 0; i < results.length; i++) {
+      if (results[i].byline != "") {
+        filteredResults.unshift(results[i]);
       }
+    }
+    updateDB(filteredResults);
+  }
 
-    
+  scrapeNYT();
 
-    // If we were able to successfully scrape and save an Article, send a message to the client
-    res.send("Scrape Complete");
+  // Create a new Article using the `result` object built from scraping
+  function updateDB(filteredResults) {
+    db.Article.create(filteredResults)
+      .then(function (dbArticle) {
+        // View the added result in the console
+        console.log(dbArticle);
+      })
+      .catch(function (err) {
+        // If an error occurred, send it to the client
+        return res.json(err);
+      });
+  }
+
+
+
+  // If we were able to successfully scrape and save an Article, send a message to the client
+  res.send("Scrape Complete");
+  return res.redirect('/');
 
 });
 
 // Route for getting all Articles from the db
-app.get("/articles", function(req, res) {
+app.get("/articles", function (req, res) {
   // Grab every document in the Articles collection
-  db.Article.find({})
-    .then(function(dbArticle) {
+  db.Article.find({}).sort({
+      "_id": -1
+    })
+    .then(function (dbArticle) {
       // If we were able to successfully find Articles, send them back to the client
       res.json(dbArticle);
     })
-    .catch(function(err) {
+    .catch(function (err) {
       // If an error occurred, send it to the client
       res.json(err);
     });
+
 });
 
 // Route for grabbing a specific Article by id, populate it with it's note
-app.get("/articles/:id", function(req, res) {
+app.get("/articles/:id", function (req, res) {
   // Using the id passed in the id parameter, prepare a query that finds 
   // the matching one in our db...
-  db.Article.findOne({ _id: req.params.id })
+  db.Article.findOne({
+      _id: req.params.id
+    })
     // ..and populate all of the notes associated with it
     .populate("note")
-    .then(function(dbArticle) {
+    .then(function (dbArticle) {
       // If we were able to successfully find an Article with the given id, 
       // send it back to the client
       res.json(dbArticle);
     })
-    .catch(function(err) {
+    .catch(function (err) {
       // If an error occurred, send it to the client
       res.json(err);
     });
 });
 
 // Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
+app.post("/articles/:id", function (req, res) {
   // Create a new note and pass the req.body to the entry
   db.Note.create(req.body)
-    .then(function(dbNote) {
+    .then(function (dbNote) {
       // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.Article.findOneAndUpdate(
-        { _id: req.params.id }, 
-        { note: dbNote._id }, 
-        { new: true });
+      return db.Article.findOneAndUpdate({
+        _id: req.params.id
+      }, {
+        note: dbNote._id
+      }, {
+        new: true
+      });
     })
-    .then(function(dbArticle) {
+    .then(function (dbArticle) {
       // If we were able to successfully update an Article, send it back to the client
       res.json(dbArticle);
     })
-    .catch(function(err) {
+    .catch(function (err) {
       // If an error occurred, send it to the client
       res.json(err);
     });
 });
 
 // Start the server
-app.listen((process.env.PORT || 5000), function() {
+app.listen((process.env.PORT || 5000), function () {
   console.log("App running on port " + PORT + "!");
 });
